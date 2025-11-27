7fa7db5be9b23d2f6960bebf22fd1f5d832619f0
radu_dell <radu141@gmail.com>
2025-11-27 13:54:31 +0100

    Splits controller and solver files

--- lsy_drone_racing/control/attitude_mpc.py
+++ lsy_drone_racing/control/attitude_mpc.py
@@ -12,166 +12,17 @@ from __future__ import annotations  # Python 3.10 type hints
 from typing import TYPE_CHECKING
 
 import numpy as np
-import scipy
-from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
 from drone_models.core import load_params
-from drone_models.so_rpy import symbolic_dynamics_euler
 from drone_models.utils.rotation import ang_vel2rpy_rates
+from pmm_planner.utils import plan_pmm_trajectory
 from scipy.spatial.transform import Rotation as R
 
-from pmm_planner.utils import plan_pmm_trajectory
-
-import csv
-import os
-
-from lsy_drone_racing.utils.utils import draw_line
-
 from lsy_drone_racing.control import Controller
+from lsy_drone_racing.control.ocp_solver import create_ocp_solver
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
-
-def create_acados_model(parameters: dict) -> AcadosModel:
-    """Creates an acados model from a symbolic drone_model."""
-    X_dot, X, U, _ = symbolic_dynamics_euler(
-        mass=parameters["mass"],
-        gravity_vec=parameters["gravity_vec"],
-        J=parameters["J"],
-        J_inv=parameters["J_inv"],
-        acc_coef=parameters["acc_coef"],
-        cmd_f_coef=parameters["cmd_f_coef"],
-        rpy_coef=parameters["rpy_coef"],
-        rpy_rates_coef=parameters["rpy_rates_coef"],
-        cmd_rpy_coef=parameters["cmd_rpy_coef"],
-    )
-
-    # Initialize the nonlinear model for NMPC formulation
-    model = AcadosModel()
-    model.name = "basic_example_mpc"
-    model.f_expl_expr = X_dot
-    model.f_impl_expr = None
-    model.x = X
-    model.u = U
-
-    return model
-
-
-def create_ocp_solver(
-    Tf: float, N: int, parameters: dict, verbose: bool = False
-) -> tuple[AcadosOcpSolver, AcadosOcp]:
-    """Creates an acados Optimal Control Problem and Solver."""
-    ocp = AcadosOcp()
-
-    # Set model
-    ocp.model = create_acados_model(parameters)
-
-    # Get Dimensions
-    nx = ocp.model.x.rows()
-    nu = ocp.model.u.rows()
-    ny = nx + nu
-    ny_e = nx
-
-    # Set dimensions
-    ocp.solver_options.N_horizon = N
-
-    ## Set Cost
-    # For more Information regarding Cost Function Definition in Acados:
-    # https://github.com/acados/acados/blob/main/docs/problem_formulation/problem_formulation_ocp_mex.pdf
-    #
-
-    # Cost Type
-    ocp.cost.cost_type = "LINEAR_LS"
-    ocp.cost.cost_type_e = "LINEAR_LS"
-
-    # Weights
-    # State weights
-    Q = np.diag(
-        [
-            50.0,  # pos
-            50.0,  # pos
-            100.0,  # pos
-            1.0,  # rpy
-            1.0,  # rpy
-            1.0,  # rpy
-            10.0,  # vel
-            10.0,  # vel
-            10.0,  # vel
-            5.0,  # drpy
-            5.0,  # drpy
-            5.0,  # drpy
-        ]
-    )
-    # Input weights (reference is upright orientation and hover thrust)
-    R = np.diag(
-        [
-            1.0,  # rpy
-            1.0,  # rpy
-            1.0,  # rpy
-            50.0,  # thrust
-        ]
-    )
-
-    # -------- Cost formulation --------------------
-    Q_e = Q.copy()
-    ocp.cost.W = scipy.linalg.block_diag(Q, R)
-    ocp.cost.W_e = Q_e
-
-    Vx = np.zeros((ny, nx))
-    Vx[0:nx, 0:nx] = np.eye(nx)  # Select all states
-    ocp.cost.Vx = Vx
-
-    Vu = np.zeros((ny, nu))
-    Vu[nx : nx + nu, :] = np.eye(nu)  # Select all actions
-    ocp.cost.Vu = Vu
-
-    Vx_e = np.zeros((ny_e, nx))
-    Vx_e[0:nx, 0:nx] = np.eye(nx)  # Select all states
-    ocp.cost.Vx_e = Vx_e
-
-    # Set initial references (we will overwrite these later on to make the controller track the traj.)
-    ocp.cost.yref, ocp.cost.yref_e = np.zeros((ny,)), np.zeros((ny_e,))
-
-    # ----------- Constraint formulation ---------------
-
-    # # Set State Constraints (rpy < 30°)
-    # ocp.constraints.lbx = np.array([-0.5, -0.5, -0.5])
-    # ocp.constraints.ubx = np.array([0.5, 0.5, 0.5])
-    # ocp.constraints.idxbx = np.array([3, 4, 5])
-
-    # Set Input Constraints (rpy < 30°)
-    ocp.constraints.lbu = np.array([-0.5, -0.5, -0.5, parameters["thrust_min"] * 4])
-    ocp.constraints.ubu = np.array([0.5, 0.5, 0.5, parameters["thrust_max"] * 4])
-    ocp.constraints.idxbu = np.array([0, 1, 2, 3])
-
-    # We have to set x0 even though we will overwrite it later on.
-    ocp.constraints.x0 = np.zeros((nx))
-
-    # Solver Options
-    ocp.solver_options.qp_solver = "FULL_CONDENSING_HPIPM"  # FULL_, PARTIAL_ ,_HPIPM, _QPOASES
-    ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
-    ocp.solver_options.integrator_type = "ERK"
-    ocp.solver_options.nlp_solver_type = "SQP"  # SQP, SQP_RTI
-    ocp.solver_options.tol = 1e-6
-
-    ocp.solver_options.qp_solver_cond_N = N
-    ocp.solver_options.qp_solver_warm_start = 1
-
-    ocp.solver_options.qp_solver_iter_max = 20
-    ocp.solver_options.nlp_solver_max_iter = 50
-
-    # set prediction horizon
-    ocp.solver_options.tf = Tf
-
-    acados_ocp_solver = AcadosOcpSolver(
-        ocp,
-        json_file="c_generated_code/lsy_example_mpc.json",
-        verbose=verbose,
-        build=True,
-        generate=True,
-    )
-
-    return acados_ocp_solver, ocp
 
 
 class AttitudeMPC(Controller):
