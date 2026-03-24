d85b62f6515c517e98d685b7d8055084f0dbcfba
radu_dell <radu141@gmail.com>
2026-03-24 09:36:02 +0100

    Fixes typo

--- tools/setup_mocap.sh
+++ tools/setup_mocap.sh
@@ -7,7 +7,7 @@ if [ ! -d ros_ws/src/motion_capture_tracking/.git ]; then
   git clone --recurse-submodules https://github.com/utiasDSL/motion_capture_tracking ros_ws/src/motion_capture_tracking
 fi
 
-if [ ! -d ros_ws/src/motion_capture_tracking/.git ]; then
+if [ ! -d ros_ws/src/drone_racing_msgs/.git ]; then
   echo "[Pixi activation] Cloning motion_capture_tracking..."
   git clone --recurse-submodules https://github.com/rducrist/drone_racing_msgs.git ros_ws/src/drone_racing_msgs
 fi
